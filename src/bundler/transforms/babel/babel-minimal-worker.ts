11fb2b91816812a751818cb5393f9dcb9579c94a
Danilo Woznica <danilowoz@gmail.com>
2022-10-07 09:03:29 +0000

    Update babel-minimal-worker.ts and yarn.lock

--- src/bundler/transforms/babel/babel-minimal-worker.ts
+++ src/bundler/transforms/babel/babel-minimal-worker.ts
@@ -1,11 +1,12 @@
-
-import {transformSync} from "@babel/core"
-import presetReact from "@babel/preset-react";
+import { transformSync } from '@babel/core';
+import type { PluginItem } from '@babel/core';
+import presetReact from '@babel/preset-react';
 
 import * as logger from '../../../utils/logger';
 import { WorkerMessageBus } from '../../../utils/WorkerMessageBus';
 import { ITranspilationResult } from '../Transformer';
-
+import { loadPlugin } from './babel-plugin-registry';
+import { collectDependencies } from './dep-collector';
 
 export interface ITransformData {
   code: string;
@@ -13,32 +14,65 @@ export interface ITransformData {
   config: any;
 }
 
+function getNameFromConfigEntry(entry: any): string | null {
+  if (typeof entry === 'string') {
+    return entry;
+  } else if (Array.isArray(entry) && typeof entry[0] === 'string') {
+    return entry[0];
+  } else {
+    return null;
+  }
+}
 
+async function getPlugins(plugins: any): Promise<PluginItem[]> {
+  const result: PluginItem[] = [];
+  if (!Array.isArray(plugins)) {
+    return result;
+  }
+  for (const plugin of plugins) {
+    const pluginName = getNameFromConfigEntry(plugin);
+    if (pluginName !== null) {
+      if (!babel.availablePlugins[pluginName]) {
+        babel.availablePlugins[pluginName] = await loadPlugin(pluginName);
+      }
 
+      const foundIndex = result.findIndex((v) => getNameFromConfigEntry(v) === pluginName);
+      if (foundIndex > -1) {
+        result[foundIndex] = plugin;
+        continue;
+      }
+    }
+    result.push(plugin);
+  }
+  return result;
+}
 
 async function transform({ code, filepath, config }: ITransformData): Promise<ITranspilationResult> {
+  const requires: Set<string> = new Set();
+
+  requires.add('react-refresh/runtime');
+  // const plugins = await getPlugins(config?.plugins ?? []);
+  // plugins.push(collectDependencies(requires));
 
   let transformed = transformSync(code, {
     filename: filepath,
-    presets:[presetReact],
-    plugins:[],
+    presets: [presetReact],
+    plugins: [],
     // no ast needed for now
     ast: false,
     sourceMaps: 'inline',
     compact: /node_modules/.test(filepath),
   });
-  
-  console.log(transformed)
 
   // no-op module
   if (!transformed?.code) {
-      transformed = {}
+    transformed = {};
     transformed.code = 'module.exports = {};';
   }
 
   return {
-    code: transformed?.code??"",
-    dependencies: new Set()
+    code: transformed?.code ?? '',
+    dependencies: requires,
   };
 }
 
